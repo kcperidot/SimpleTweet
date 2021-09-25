ff12832a7b8463b8b2e2c45b293fb0cc5d234319
kcperidot <kcperidot@gmail.com>
2021-09-24 21:46:30 -0700

    commit infinite scroll

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -61,12 +61,14 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
         ImageView ivProfileImage;
         TextView tvBody;
         TextView tvScreenName;
+        TextView tvTimestamp;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             ivProfileImage = itemView.findViewById(R.id.ivProfileImage);
             tvBody = itemView.findViewById((R.id.tvBody));
             tvScreenName = itemView.findViewById(R.id.tvScreenName);
+            tvTimestamp = itemView.findViewById(R.id.tvTimestamp);
         }
 
         public void bind(Tweet tweet) {
--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -1,5 +1,7 @@
 package com.codepath.apps.restclienttemplate.models;
 
+import com.codepath.apps.restclienttemplate.TimeFormatter;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -10,14 +12,23 @@ import java.util.List;
 public class Tweet {
     public String body;
     public String createdAt;
+    public long id;
     public User user;
+    public String timestamp;
 
     public static Tweet fromJson(JSONObject jsonObject) throws JSONException {
         Tweet tweet = new Tweet();
         tweet.body = jsonObject.getString("text");
         tweet.createdAt = jsonObject.getString("created_at");
+        tweet.id = jsonObject.getLong("id");
         tweet.user = User.fromJson(jsonObject.getJSONObject("user"));
+        tweet.timestamp = getFormattedTimestamp(jsonObject);
         return tweet;
+    }
+
+    public static String getFormattedTimestamp(JSONObject jsonObject) throws JSONException {
+        String createdAt = jsonObject.getString("created_at");
+        return TimeFormatter.getTimeDifference(createdAt);
     }
 
     public static List<Tweet> fromJsonArray(JSONArray jsonArray) throws JSONException {
