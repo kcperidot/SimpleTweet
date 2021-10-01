39006420bef0e7feb92f8f368138e0a30e494969
kcperidot <kcperidot@gmail.com>
2021-10-01 14:08:12 -0700

    Update: character counter edit

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -41,25 +41,6 @@ public class ComposeActivity extends AppCompatActivity {
         btnTweet = findViewById(R.id.btnTweet);
         tiWordCount = findViewById(R.id.tiWordCount);
 
-        //character counter
-        etCompose.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                // Fires right as the text is being changed (even supplies the range of text)
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count,
-                                          int after) {
-                // Fires right before text is changing
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                // Fires right after the text has changed
-            }
-        });
-
         //Set click listener on button
         btnTweet.setOnClickListener(new View.OnClickListener() {
             @Override
